ab828066b8a8aad40de3589c08a7dd146e43e2e7
askidea <askinnovativelab@gmail.com>
2025-11-17 19:50:28 +0530

    vercel commits

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Helmet } from 'react-helmet-async';
+import { Helmet } from 'react-helmet';
 
 const SEO = ({ 
   title = "Click Nova - Best Digital Marketing Agency | Social Media, Google Ads & Website Development",
